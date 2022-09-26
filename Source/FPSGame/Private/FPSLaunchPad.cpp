2d1b47030092a6dcca9465e62c6f87dc0e4198da
witmaos6 <geonhee518@naver.com>
2022-09-26 22:49:22 +0900

    Add LaunchPad C++

---
+++ Source/FPSGame/Private/FPSLaunchPad.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSLaunchPad.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/ArrowComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AFPSLaunchPad::AFPSLaunchPad()
+{
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetBoxExtent(FVector(75, 75, 50));
+	RootComponent = OverlapComp;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetupAttachment(RootComponent);
+
+	LaunchStrength = 1500;
+	LaunchPitchAngle = 35.0f;
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSLaunchPad::OverlapLaunchPad);
+}
+
+void AFPSLaunchPad::OverlapLaunchPad(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	FRotator LaunchDirection = GetActorRotation();
+	LaunchDirection.Pitch += LaunchPitchAngle;
+	FVector LaunchVelocity = LaunchDirection.Vector() * LaunchStrength;
+
+	ACharacter* OtherCharacter = Cast<ACharacter>(OtherActor);
+	if(OtherCharacter)
+	{
+		OtherCharacter->LaunchCharacter(LaunchVelocity, true, true);
+
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ActivateLaunchPadEffect, GetActorLocation());
+	}
+	else if(OtherComp && OtherComp->IsSimulatingPhysics())
+	{
+		OtherComp->AddImpulse(LaunchVelocity, NAME_None, true);
+
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ActivateLaunchPadEffect, GetActorLocation());
+	}
+}
+
